529b2f195bdad5c2744f32e729c54be056ff7966
VighneshBilgi <vhbilgi@gmail.com>
2022-08-31 14:09:29 +0530

    Added: UC2_Create_File

--- src/com/bridgelabz/fileioexample/FileIOExample.java
+++ src/com/bridgelabz/fileioexample/FileIOExample.java
@@ -7,17 +7,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileIOExample {
-
+    //1. Set Directory Path
     public static final String DIR_PATH = "C:/Users/bilgi/IdeaProjects/Java Remap/FileIOExample/TestDirectory";
+    //4. Set File Path
+    public static final String FILE_PATH = DIR_PATH + "/TestFile.txt";
 
     public static void main(String[] args) {
         System.out.println("File IO Example:");
-        //2. Set the path to the reference of I/F "Path" with Paths.get()
+        //2. Set the directory path to the reference of I/F "Path" with Paths.get()
         Path directoryPath = Paths.get(DIR_PATH);
+        //5. Set the file path to the reference of I/F "Path" with Paths.get()
+        Path filePath = Paths.get(FILE_PATH);
 
-        //3.a. Check if directoryPath exists , if not then create path with Files.exists().
-
+        //3.a. Check if directoryPath exists Files.exists(), if not then create path with Files.createDirectories().
         System.out.println("Does "+DIR_PATH+" exist ? " + Files.exists(directoryPath));
+        //6.a. Check if filePath exists Files.exists(), if not then create path with Files.createFile().
+        System.out.println("Does "+FILE_PATH+" exist ? " + Files.exists(filePath));
 
         if(Files.notExists(directoryPath)){
             //3.b. NOTE: Compiler will not allow you to create a directory/file/path for directory or file until
@@ -31,8 +36,21 @@ public class FileIOExample {
                 e.printStackTrace();
             }
 
+        }else{
+
+            if(Files.notExists(filePath)) {
+                //6.b. Much like in Step 3.b., You have to create the file , if the file doesn't exist.
+
+                try {
+                    Files.createFile(filePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
         }
 
         System.out.println("Does "+DIR_PATH+" exist ? " + Files.exists(directoryPath));
+        System.out.println("Does "+FILE_PATH+" exist ? " + Files.exists(filePath));
     }
 }
